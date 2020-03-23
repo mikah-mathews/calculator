268ca8f2cd72999266c3eefe0e88a0435435b514
Mikah Mathews <mikah@outofband.org>
2020-03-23 11:24:33 -0700

    Add add, subtract, multiply, divide functions, get user input for numbers,print results on page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
 var add = function(number1, number2) {
     return number1 + number2;
   };
-  add(12, 24);
   
-  var number1 = parseInt(prompt("Pick a number:"));
-  var number2 = parseInt(prompt("Pick another number:"));
-  alert(add(number1, number2));
+var subtract = function(number1, number2) {
+    return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+    return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+    return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Pick a number:"));
+var number2 = parseInt(prompt("Pick another number:"));
+
+var addresult = add(number1, number2);
+var subresult = subtract(number1, number2);
+var mulresult = multiply(number1, number2);
+var divresult = divide(number1, number2);
+
+alert(addresult);
+alert(subresult);
+alert(mulresult);
+alert(divresult);
 
