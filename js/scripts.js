01e982886c310ca5015b720d4403f8404bb4cd3c
Mikah Mathews <mikah@outofband.org>
2020-03-23 11:02:38 -0700

    Add index.html, README.md, JS directory, CSS directory, JS/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  add(12, 24);
+  
+  var number1 = parseInt(prompt("Pick a number:"));
+  var number2 = parseInt(prompt("Pick another number:"));
+  alert(add(number1, number2));
+
